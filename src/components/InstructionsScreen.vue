96bf74f2c2c12dc317b702277cdd01b905e505c8
shree <shreepateldev@gmail.com>
2024-09-22 15:14:04 -0400

    Add gifs to instructions page

--- src/components/InstructionsScreen.vue
+++ src/components/InstructionsScreen.vue
@@ -2,15 +2,14 @@
     <div class="instructions-container">
         <p>The computer will generate a secret code. It is your job as the player to guess what the secret code is.</p>
         <p style="display: flex;">To make a guess, click on the colored pegs to add it to the current row.</p>
-        <p>With each row, you will get feedback from the computer on your guesses via the smaller pegs on the right side of the row.</p>
-        <p>The black peg indicates that one of your pegs is an incorrect guess</p>
-        <Peg :keyPeg="true" />
-        <p>The orange peg indicates that one of your pegs is the correct color but in the wrong location</p>
-        <Peg :keyPeg="true" :colorCode="'O'" />
-        <p>The green peg indiciates that you have guessed a peg correctly. It is the right color and location.</p>
-        <Peg :keyPeg="true" :colorCode="'G'" />
-        <button @click="goToStart">Back</button>
+        <img src="../assets/peg-selection.gif" />
+        <p>By clicking on the check button, the computer will give feedback on your guess via the smaller pegs on the right side of the row.</p>
+        <img src="../assets/check-row.gif" />
+        <p class="peg-instruction-container"><Peg :keyPeg="true" class="peg-instruction"/> The black peg indicates that one of your pegs is an incorrect guess</p>
+        <p class="peg-instruction-container"><Peg :keyPeg="true" :colorCode="'O'" class="peg-instruction" /> The orange peg indicates that one of your pegs is the correct color but in the wrong location</p>
+        <p class="peg-instruction-container"><Peg :keyPeg="true" :colorCode="'G'" class="peg-instruction" /> The green peg indiciates that you have guessed a peg correctly. It is in the right color and location.</p>
     </div>
+    <button @click="goToStart">Back</button>
 </template>
 
 <script setup lang="ts">
@@ -28,6 +27,7 @@ const goToStart = () => boardStore.screenState = 'Start';
     margin: 0 auto;
     font-family: 'Open Sans', sans-serif;
     font-size: 14px;
+    overflow: scroll;
 }
 
 button {
@@ -51,5 +51,14 @@ button {
 button:hover {
   background: #042297;
 	color: #fff827;
+}
+
+.peg-instruction-container {
+    display: flex;
+}
+
+.peg-instruction {
+    margin-right: 5px;
+    margin-top: 3px;
 }
 </style>
